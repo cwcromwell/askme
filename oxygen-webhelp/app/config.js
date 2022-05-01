06ba9f6eea53abd43af8c7183a8854e128485394
Clarence Cromwell <clarencecromwell@Clarences-MBP.attlocal.net>
2022-05-01 15:28:35 -0700

    April release with google analytics tags added

--- oxygen-webhelp/app/config.js
+++ oxygen-webhelp/app/config.js
@@ -110,7 +110,7 @@ define(function() {
         	
             if(id === 'index-1' || id === 'index-2' || id === 'index-3' || id === 'stopwords' 
             ||id === 'htmlFileInfoList' || id === 'keywords') {
-                args = '20220430161235';
+                args = '20220501152046';
         	} 
 	
 	        return (url.indexOf('?') === -1 ? '?' : '&') + args;
