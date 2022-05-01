0a7c952114455bbef1a7f821336344a4b855d1ee
Clarence Cromwell <clarencecromwell@Clarences-MBP.attlocal.net>
2022-04-30 22:19:37 -0700

    copied April site update into repo

---
+++ oxygen-webhelp/app/nav-links/json/tocId-d249e12.js
@@ -0,0 +1,1 @@
+define({"topics":[{"title":"Documentation design and quality","href":"topics\/minimum-viable-docset.html","menu":{"hasChildren":true},"tocID":"tocId-d249e18","next":"tocId-d249e18"},{"title":"Resources","href":"topics\/developer-resources.html","menu":{"hasChildren":true},"tocID":"tocId-d249e79","next":"tocId-d249e79"}]});
--- oxygen-webhelp/app/search/index/keywords.js
+++ oxygen-webhelp/app/search/index/keywords.js
@@ -1,16 +1,18 @@
-define(function() {var keywords=[{w:"What",p:["p0"]},{w:"are",p:["p0","p3"]},{w:"APIs",p:["p0","p5"]},{w:"and",p:["p0","p10"]},{w:"why",p:["p0"]},{w:"do",p:["p0"]},{w:"they",p:["p0"]},{w:"need",p:["p0"]},{w:"special",p:["p0"]},{w:"documentation?",p:["p0"]},{w:"About",p:["p1","p2"]},{w:"Clarence",p:["p1"]},{w:"William",p:["p1"]},{w:"Cromwell",p:["p1"]},{w:"this",p:["p2"]},{w:"site",p:["p2"]},{w:"The",p:["p3","p6","p7"]},{w:"API",p:["p3","p10"]},{w:"reference",p:["p3","p8"]},{w:"docs:",p:["p3","p9"]},{w:"yours",p:["p3"]},{w:"complete?",p:["p3"]},{w:"Resources",p:["p4","p10"]},{w:"for",p:["p4","p5","p10"]},{w:"developers",p:["p4"]},{w:"who",p:["p4"]},{w:"write",p:["p4"]},{w:"docs",p:["p4"]},{w:"Minimum",p:["p5"]},{w:"viable",p:["p5"]},{w:"documentation",p:["p5"]},{w:"Non-reference",p:["p6"]},{w:"documentation:",p:["p6"]},{w:"Writing",p:["p6"]},{w:"overview",p:["p6"]},{w:"Quickstart",p:["p7"]},{w:"Example",p:["p8"]},{w:"entry",p:["p8"]},{w:"What\'s",p:["p9"]},{w:"missing",p:["p9"]},{w:"from",p:["p9"]},{w:"your",p:["p9"]},{w:"Swagger",p:["p9"]},{w:"How",p:["p9"]},{w:"to",p:["p9"]},{w:"ensure",p:["p9"]},{w:"completeness",p:["p9"]},{w:"when",p:["p9"]},{w:"working",p:["p9"]},{w:"with",p:["p9"]},{w:"OpenAPI",p:["p9"]},{w:"3",p:["p9"]},{w:"technical",p:["p10"]},{w:"writers",p:["p10"]},{w:"in",p:["p10"]},{w:"general",p:["p10"]},{w:"particular",p:["p10"]}];
+define(function() {var keywords=[{w:"What",p:["p0"]},{w:"are",p:["p0","p3"]},{w:"APIs",p:["p0","p7"]},{w:"and",p:["p0","p12"]},{w:"why",p:["p0"]},{w:"do",p:["p0"]},{w:"they",p:["p0"]},{w:"need",p:["p0"]},{w:"special",p:["p0"]},{w:"documentation?",p:["p0"]},{w:"About",p:["p1","p2"]},{w:"Clarence",p:["p1"]},{w:"William",p:["p1"]},{w:"Cromwell",p:["p1"]},{w:"this",p:["p2"]},{w:"site",p:["p2"]},{w:"The",p:["p3","p4","p5","p8","p9"]},{w:"API",p:["p3","p12"]},{w:"reference",p:["p3","p10"]},{w:"docs:",p:["p3","p11"]},{w:"yours",p:["p3"]},{w:"complete?",p:["p3"]},{w:"Beyond",p:["p4"]},{w:"MVD:",p:["p4"]},{w:"Expanding",p:["p4"]},{w:"your",p:["p4","p5","p11"]},{w:"documentation",p:["p4","p7"]},{w:"as",p:["p4"]},{w:"product",p:["p4"]},{w:"matures",p:["p4"]},{w:"process",p:["p5"]},{w:"for",p:["p5","p6","p7","p12"]},{w:"completing",p:["p5"]},{w:"MVD",p:["p5"]},{w:"project",p:["p5"]},{w:"Resources",p:["p6","p12"]},{w:"developers",p:["p6"]},{w:"who",p:["p6"]},{w:"write",p:["p6"]},{w:"docs",p:["p6"]},{w:"Minimum",p:["p7"]},{w:"viable",p:["p7"]},{w:"Non-reference",p:["p8"]},{w:"documentation:",p:["p8"]},{w:"Writing",p:["p8"]},{w:"overview",p:["p8"]},{w:"Quickstart",p:["p9"]},{w:"Example",p:["p10"]},{w:"entry",p:["p10"]},{w:"What\'s",p:["p11"]},{w:"missing",p:["p11"]},{w:"from",p:["p11"]},{w:"Swagger",p:["p11"]},{w:"How",p:["p11"]},{w:"to",p:["p11"]},{w:"ensure",p:["p11"]},{w:"completeness",p:["p11"]},{w:"when",p:["p11"]},{w:"working",p:["p11"]},{w:"with",p:["p11"]},{w:"OpenAPI",p:["p11"]},{w:"3",p:["p11"]},{w:"technical",p:["p12"]},{w:"writers",p:["p12"]},{w:"in",p:["p12"]},{w:"general",p:["p12"]},{w:"particular",p:["p12"]}];
 var ph={};
 ph["p0"]=[0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 ph["p1"]=[10, 11, 12, 13];
 ph["p2"]=[10, 14, 15];
 ph["p3"]=[16, 17, 18, 19, 1, 20, 21];
-ph["p4"]=[22, 23, 24, 25, 26, 27];
-ph["p5"]=[28, 29, 30, 23, 2];
-ph["p6"]=[31, 32, 33, 16, 34];
-ph["p7"]=[16, 35];
-ph["p8"]=[36, 18, 37];
-ph["p9"]=[38, 39, 40, 41, 42, 19, 43, 44, 45, 46, 47, 48, 49, 50, 51];
-ph["p10"]=[22, 23, 52, 53, 54, 55, 3, 17, 53, 54, 56];
+ph["p4"]=[22, 23, 24, 25, 26, 27, 16, 28, 29];
+ph["p5"]=[16, 30, 31, 32, 25, 33, 34];
+ph["p6"]=[35, 31, 36, 37, 38, 39];
+ph["p7"]=[40, 41, 26, 31, 2];
+ph["p8"]=[42, 43, 44, 16, 45];
+ph["p9"]=[16, 46];
+ph["p10"]=[47, 18, 48];
+ph["p12"]=[35, 31, 62, 63, 64, 65, 3, 17, 63, 64, 66];
+ph["p11"]=[49, 50, 51, 25, 52, 19, 53, 54, 55, 56, 57, 58, 59, 60, 61];
      return {
          keywords: keywords,
          ph: ph
