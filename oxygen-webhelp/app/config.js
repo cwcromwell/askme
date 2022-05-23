aa48f1441f14db9689ae5d11a6991c23c6eabb6d
Clarence Cromwell <clarencecromwell@Clarences-MBP.attlocal.net>
2022-05-22 20:24:39 -0700

    updated swagger-reference page and promoted 5-22-22

--- oxygen-webhelp/app/config.js
+++ oxygen-webhelp/app/config.js
@@ -110,7 +110,7 @@ define(function() {
         	
             if(id === 'index-1' || id === 'index-2' || id === 'index-3' || id === 'stopwords' 
             ||id === 'htmlFileInfoList' || id === 'keywords') {
-                args = '20220514135244';
+                args = '20220522201852';
         	} 
 	
 	        return (url.indexOf('?') === -1 ? '?' : '&') + args;
