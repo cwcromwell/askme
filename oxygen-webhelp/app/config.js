d51f92987efb3e6190a643936b282a798d8cf37e
Clarence Cromwell <clarencewcromwell@gmail.com>
2023-08-15 06:30:44 -0700

    askme site updates

--- oxygen-webhelp/app/config.js
+++ oxygen-webhelp/app/config.js
@@ -110,7 +110,7 @@ define(function() {
         	
             if(id === 'index-1' || id === 'index-2' || id === 'index-3' || id === 'stopwords' 
             ||id === 'htmlFileInfoList' || id === 'keywords') {
-                args = '20230806155838';
+                args = '20230815062645';
         	} 
 	
 	        return (url.indexOf('?') === -1 ? '?' : '&') + args;
