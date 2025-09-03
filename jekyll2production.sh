a4efa2b84df4afb68ba30090a23ad877fc1dd9ee
Clarence Cromwell <clarencewcromwell@gmail.com>
2025-09-02 22:46:35 -0700

    Rebuild Jekyll and add new Google Analytics tag

--- jekyll2production.sh
+++ jekyll2production.sh
@@ -3,7 +3,7 @@
 echo "Trying to copy to askme repo."
 echo "File move is verified" >> _site/askmeverify.txt 
 cp -R _site/* ../askme-site/askme  || echo "UNABLE TO COPY FILES. Does the directory exist?"
-cat ../askme-site/askmeverify.txt  ||  echo "COPYING OPERATION FAILED."
+cat ../askme-site/askme/askmeverify.txt  ||  echo "COPYING OPERATION FAILED."
 # Fix: Use && or ; instead of | for sequential commands
-rm _site/askmeverify.txt  && rm ../askme-site/askmeverify.txt
+rm _site/askmeverify.txt  && rm ../askme-site/askme/askmeverify.txt
 
